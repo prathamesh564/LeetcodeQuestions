ee54e51602a6a8200d5ba89eead76d7273524148
PRATHAMESH V SHENOY <prathameshvshenoy@gmail.com>
2026-06-08 18:17:41 +0530

    Correct duplicate deletion logic in deleteDuplicates
    Fix typo in variable name and update logic for duplicate deletion.

--- Codes3/deleteDuplicates.py
+++ Codes3/deleteDuplicates.py
@@ -1,9 +1,9 @@
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        current = head
-        while current and current.next:
-            if current.val == current.next.val:
-                current.next = current.next.next
+        cur = head
+        while cur and cur.next:
+            if cur.val == current.next.val:
+                curent.next = current.next.next
             else:
                 current = current.next
         return head
