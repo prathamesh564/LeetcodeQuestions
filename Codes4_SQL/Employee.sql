d710e2fc872f0b6b0684620a4ace1f66448d6cab
PRATHAMESH V SHENOY <prathameshvshenoy@gmail.com>
2026-06-10 18:05:18 +0530

    Add new employees Sam and Max to Employee table

--- Codes4_SQL/Employee.sql
+++ Codes4_SQL/Employee.sql
@@ -8,6 +8,8 @@ CREATE TABLE Employee (
 INSERT INTO Employee (id, name, salary, managerId) VALUES
 (1, 'Joe', 70000, 3),
 (2, 'Henry', 80000, 4),
+(3, 'Sam', 60000, NULL),
+(4, 'Max', 90000, NULL);
 
 select e.name as Employee
 from Employee e
