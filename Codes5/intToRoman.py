fe238da7783c5033fc4b1ce399412ad771ee322f
PRATHAMESH V SHENOY <prathameshvshenoy@gmail.com>
2026-06-22 21:28:57 +0530

    Implement intToRoman function for integer conversion

--- Codes5/intToRoman.py
+++ Codes5/intToRoman.py
@@ -1,2 +1,17 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
+        roman_map = [
+            (1000,"M"),(900,"CM"),(500,"D"),(400,"CD"),
+            (100,"C"),(90,"XC"),(50,"L"),(40,"XL"),
+            (10,"X"),(9,"IX"),(5,"V"),(4,"IV"),
+            (1,"I")
+        ]
+        result=[]
+        for val,sym in roman_map:
+            if num==0:
+                break
+            count=num//val
+            if count>0:
+                result.append(sym*count)
+                num-=val*count
+        return "".join(result)
